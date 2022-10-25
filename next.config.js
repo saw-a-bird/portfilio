2196886da958f6e26be53ab9995bd780c7ad9dd8
pokerfce <gamezrookie@gmail.com>
2022-10-26 00:12:59 +0100

    in dev

---
+++ next.config.js
@@ -0,0 +1,18 @@
+/** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === "production";
+
+module.exports = {
+  /**
+   * Tell Next.js where the `public` folder is.
+   * Replace `nextjs-github-pages` with your Github repo project name.
+   */
+  assetPrefix: isProd ? "/mywebsite/" : "",
+  /**
+   * Disable server-based image optimization.
+   *
+   * @see https://nextjs.org/blog/next-12-3#disable-image-optimization-stable
+   */
+  images: {
+    unoptimized: true,
+  },
+};
