820a3addb7dc0147cbe0acea23e35a7f1dbc961e
pokerfce <gamezrookie@gmail.com>
2022-10-27 11:11:17 +0100

    added mobile css

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,22 @@ module.exports = {
   theme: {
     minWidth: {
       '1/3': '33%'
+    },
+    screens: {
+      'sm': {'max': '640px'},
+      // => @media (min-width: 640px) { ... }
+
+      'md': {'max': '768px'},
+      // => @media (min-width: 768px) { ... }
+
+      'lg': {'max': '1024px'},
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': {'max': '1280px'},
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': {'max': '1536px'}
+      // => @media (min-width: 1536px) { ... }
     }
   },
   plugins: [],
